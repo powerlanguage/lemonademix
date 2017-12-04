9c51f4af746a90ebd2257e653c06a88fcd4de9db
powerlanguage <josh.wardle@gmail.com>
2017-12-03 19:38:27 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+A super dumb thing that helps you remember people's names.
+
+Add images to the `assets` folder and names to the array in `lemons.js`.  The format should be as follows
+
+- name `"Josh Wardle"`
+- image file name `"josh_wardle.jpg"`
+
+🍋🍋🍋
---
+++ lemons.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+    var lemons = [
+        // put names here
+    ];
+
+    $('button').on('click', checkResults);
+
+    var select = generateSelect(lemons);
+    var randomLemons = shuffle(lemons);
+
+    randomLemons.forEach(function(lemon){
+        $("#container").append(generateLemon(lemon));
+    });
+
+    function generateSelect(lemons) {
+        var template = '<select><option value="adas">Select Name</option>';
+        lemons.forEach(function(lemon){
+            template += `<option value="${lemon}">${lemon}</option>`
+        });
+        template += "</select>";
+        return template;
+    }
+
+    function generateLemon(name) {
+      var url = name.split(" ").join("_").toLowerCase() + ".jpg";
+      var template =
+      `<div class="lemon" id="${name}"">
+        <img src="assets/${url}" />
+        ${select}
+      </div>`;
+      return template;
+    }
+
+    function checkResults(){
+        var $lemons = $(".lemon");
+        var countSuccess = 0;
+        for(var i = 0; i < lemons.length; i++) {
+            var lemon = $lemons[i];
+            var name = lemon.id;
+            var select = $(lemon).find("select")[0];
+            var selected = select[select.selectedIndex].text;
+            if(name === selected) {
+                $(lemon).removeClass("fail");
+                $(lemon).addClass("succeed");
+                countSuccess++;
+            } else {
+                $(lemon).removeClass("succeed");
+                $(lemon).addClass("fail");
+            }
+        };
+        $("#results").text(`${countSuccess}/${$lemons.length}`);
+        $("#results").show();
+    }
+
+    function shuffle(array) {
+    var results = [];
+    var copy = array.slice(0);
+    while (copy.length > 0) {
+      var randomIndex = Math.floor(Math.random() * (copy.length - 1));
+      results.push(copy[randomIndex]);
+      copy.splice(randomIndex, 1);
+    }
+    return results;
+    };
+})
